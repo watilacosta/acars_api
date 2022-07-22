7db20b19100dd9860af954752572b13f898aa8e0
watila <watilafreire26@gmail.com>
2022-07-22 18:23:12 -0300

    refact method call

--- app/interactors/create_aircraft_model.rb
+++ app/interactors/create_aircraft_model.rb
@@ -4,10 +4,14 @@ class CreateAircraftModel
   include Interactor
 
   def call
-    aircraft_model = AircraftModel.create!(context.permitted_params)
-
-    context.aircraft_model = aircraft_model
+    context.aircraft_model = create_aircraft_model
   rescue ActiveRecord::RecordInvalid => e
     context.fail!(message: e.record.errors.full_messages.join(', ').to_s)
+  end
+
+  private
+
+  def create_aircraft_model
+    AircraftModel.create!(context.permitted_params)
   end
 end
