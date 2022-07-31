130f9fc4c82b9a4dd41176389badaeefc693763c
watila <watilafreire26@gmail.com>
2022-07-31 18:11:40 -0300

    create rating started

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'ratings/create'
+  get 'ratings/update'
+  get 'ratings/destroy'
   get 'user_query/index' => 'user_query#index'
 
   resources :aircraft_models, only: %i[index create update destroy]
