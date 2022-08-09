9699965689608edff1482e7417b530547ccbff52
watila <watilafreire26@gmail.com>
2022-08-08 21:30:31 -0300

    factory fleet

--- app/models/fleet.rb
+++ app/models/fleet.rb
@@ -2,4 +2,9 @@ class Fleet < ApplicationRecord
   belongs_to :aircraft_model
   belongs_to :location, class_name: 'Airport', foreign_key: 'location_id'
   belongs_to :hub, class_name: 'Airport', foreign_key: 'hub_id'
+
+  enum status: {
+    unavailable: 0,
+    available: 1
+  }
 end
--- spec/factories/fleets.rb
+++ spec/factories/fleets.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :fleet do
-    aircraft_model { nil }
-    designator { "MyString" }
-    cover { "MyString" }
-    turbulence_category { "MyString" }
-    location { nil }
-    status { 1 }
-    hub { nil }
+    aircraft_model { create(:aircraft_model) }
+    designator { Faker::Lorem.characters(number: 4) }
+    cover { 'https://www.linktocover.com/cover.jpg' }
+    turbulence_category { %i[A B C D].sample }
+    location { create(:airport) }
+    status { Fleet.statuses.to_a.sample.last }
+    hub { create(:airport) }
   end
 end
