af3e0789bfe9afe078e93844059ece94bfb617db
Watila Costa <watila.costa@redealumni.com>
2022-08-24 18:30:09 -0300

    pireps controller init

--- app/controllers/api/version1/pireps_controller.rb
+++ app/controllers/api/version1/pireps_controller.rb
@@ -6,15 +6,19 @@ class PirepsController < ApplicationController
   private
 
   def pirep_params
-    params.slice!(:user, :pass, :auth, :version, :aircraft, :atcModel, :atcData, :lat1,
-                  :lon1, :lat2, :lon2, :latland, :lonland, :directNM, :actualNM, :dateshort,
-                  :datestamp, :timeout, :timeoff, :timeon, :timein, :fstimeout, :fstimeoff,
-                  :fstimeon, :fstimein, :blocktime, :fuelstart, :fuelstop, :fueldiff,
-                  :timoday, :landingFR, :landingKTS, :landingFPM, :takeoffLBS, :landingLBS,
-                  :pirep, :pause, :slew, :stall, :overspeed, :speed10k, :simrate, :refuel,
-                  :crashed, :nofuel, :warpNM, :MILdiffLBS, :lsotxt, :lsonum, :takeoffFR,
-                  :windstart, :windstop, :payload, :fsVersion, :oew, :zfw, :)
+    params.slice!(:user, :pass, :auth, :version, :aircraft, :atcModel, :atcData,
+                  :lat1, :lon1, :lat2, :lon2, :latland, :lonland, :directNM,
+                  :actualNM, :dateshort, :datestamp, :timeout, :timeoff, :timeon,
+                  :timein, :fstimeout, :fstimeoff, :fstimeon, :fstimein,
+                  :blocktime, :fuelstart, :fuelstop, :fueldiff, :timoday,
+                  :landingFR, :landingKTS, :landingFPM, :takeoffLBS, :landingLBS,
+                  :pirep, :pause, :slew, :stall, :overspeed, :speed10k, :simrate,
+                  :refuel, :crashed, :nofuel, :warpNM, :MILdiffLBS, :lsotxt,
+                  :lsonum, :takeoffFR, :windstart, :windstop, :payload,
+                  :fsVersion, :oew, :zfw, :rollout, :blockfuel, :enroutefuel,
+                  :taxifuel, :gmax, :gmin, :takeoffHWC, :takeoffXWC, :landingHWC,
+                  :landingXWC, :hwavg, :takeoffflaps, :landingflaps, :omaxtaxi,
+                  :dmaxtaxi, :takeoffKTS, :trueheading, :multifpm )
 
   end
-
 end
